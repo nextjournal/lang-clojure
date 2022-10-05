8344c8fe93473a70ec895fea01990bb822d016bb
Andrea Amantini <lo.zampino@gmail.com>
2022-10-05 19:07:24 +0200

    TODOS

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ import {EditorView, basicSetup, minimalSetup} from 'codemirror'
 import {clojure} from './src/clojure.ts'
 
 new EditorView({
-  doc: `
-(ns foo.bar
+  doc: `(ns foo.bar
   "this is a nice ns"
   (:require [cloure.string :as str]))
+
+;; # TODO:
+;; - [x] basic syntax
+;; - [x] syntax highlighting
+;; - [ ] fold
+;; - [ ] indent
 
 #_ #readme 123
 
@@ -19,8 +24,7 @@ new EditorView({
     a b
     true false
     :always 2
-    'else nil)
-`,
+    'else nil)`,
 
   extensions: [basicSetup,
                clojure()],
